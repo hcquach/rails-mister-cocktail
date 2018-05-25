5f8fa57052fb07c69102795a95741b8e6df47c8f
Hien-Chanh Quach <hcquach@gmail.com>
2018-05-25 09:37:45 +0900

    Prepare for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :index, :new, :create, :show ] do
+    resources :doses, only: [ :index, :new, :create, :show ]
+  end
+
+  resources :doses, only: [ :destroy ]
+  # delete "doses/:id", to: "doses#destroy", as: :delete_dose
+
+  root to: 'cocktails#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+# ingredient1 = Ingredient.create(name: "lemon")
+# ingredient2 = Ingredient.create(name: "ice")
+# ingredient3 = Ingredient.create(name: "mint leaves")
+
+# cocktail1 = Cocktail.create!(name: "lemon juice")
+
+# dose1 = Dose.create!(description: "3 lemons and water", cocktail_id: cocktail1.id, ingredient_id: ingredient1.id)
+
+Cocktail.destroy_all if Rails.env.development?
+Dose.destroy_all if Rails.env.development?
+Ingredient.destroy_all if Rails.env.development?
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredients = JSON.parse(ingredient_serialized)
+
+ingredients['drinks'].each do |hash_ingredient|
+  ingredient = hash_ingredient['strIngredient1']
+  Ingredient.create!(name: ingredient.capitalize)
+end
+
+["SAZERAC", "MOJITO", "MOSCOW MULE", "MARGARITA", "DRY MARTINI", "WHISKEY SOUR", "MANHATTAN", "DAIQUIRI", "NEGRONI", "OLD FASHIONED"].each do |cocktail|
+  Cocktail.create!(name: cocktail.capitalize)
+end
